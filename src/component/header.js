349f688f984396f8b91eea74d0c32d9d48c353b8
tivih1996 <tivih1996@gmail.com>
2024-06-06 15:00:44 +0700

    update header

--- src/component/header.js
+++ src/component/header.js
@@ -2,12 +2,12 @@ const html = `
 <header class="header">
     <a href="/index.html" class="logo">CompanyLogo</a>
     <div class="header-right a-tag-nav">
-        <a class="active" href="./show-category.html ">Show category</a>
+        <a class="active" href="./show-category.html ">Danh mục</a>
     </div>
     <div class="header-right a-tag-nav">
-        <a class="active" href="./create-new-category.html ">Create category</a>
+        <a class="active" href="./create-new-category.html ">Tạo Danh mục</a>
     </div>
-
+    
     
 </header>
 `
